ce82d8fa4c9092ea1a4d343897c055be27a0e11d
Berend Weij <berend@studiozoetekauw.nl>
2024-11-25 13:44:08 +0100

    adds hints

--- Assets/Scripts/FloodFill.cs
+++ Assets/Scripts/FloodFill.cs
@@ -19,6 +19,16 @@ public class FloodFill : MonoBehaviour
     // opdracht: laat deze method werken
     private void FloodFillArea(ColorGrid colorGrid, int x, int y, Color targetColor, Color replacementColor)
     {
-        
+        // Stap 1: als de x en y niet 'binnen' de grid width en height liggen, return dan
+
+        // Stap 2: Haal de huidige kleur van de tile op (deze x en y) en sla deze op in een variabele
+
+        // Stap 3: Stop (return) als de currentColor niet overeenkomt met de targetColor
+
+        // Stap 4: Stop (return) als de huidige kleur al de replacementColor is
+
+        // Stap 5: Verander de kleur van de huidige tile (colorGrid heeft een SetPixel functie)
+
+        // Stap 6: Roep deze functie ook recursief aan voor aangrenzende tiles
     }
 }
